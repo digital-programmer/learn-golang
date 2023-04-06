4184d1af311775e468fcc5cb7585efa3c305ffe5
dibya-sunstone <dibya.acharya@sunstone.edu.in>
2023-04-06 10:30:17 +0530

    initial learn golang repo

---
+++ chapter1/calc/calc_test.go
@@ -0,0 +1,17 @@
+package calc
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	var result int = Add(15,10)
+	if result!= 25 {
+		t.Error("Expected 25, got ", result)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	var result int = Subtract(15,10)
+	if result != 5 {
+		t.Error("Expected 5, got ", result)
+	}
+}
